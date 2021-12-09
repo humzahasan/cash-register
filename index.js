32c1f8337dc657a428f23fcb18945a5c7e72c8ea
Humza Hasan <humzahasan2101@gmail.com>
2021-12-09 18:58:05 +0530

    fix: logic refactor

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 //Selectors
-let billAmount = document.getElementById("billAmt");
-let cashGiven = document.getElementById("cash");
-let amtWarning = document.getElementById("billAmtWarn");
-let cashWarning = document.getElementById("cashWarn");
-let getChangeBtn = document.getElementById("getChangeBtn");
-let noteCount = document.querySelectorAll(".count");
+const billAmount = document.getElementById("billAmt");
+const cashGiven = document.getElementById("cash");
+const amtWarning = document.getElementById("billAmtWarn");
+const cashWarning = document.getElementById("cashWarn");
+const getChangeBtn = document.getElementById("getChangeBtn");
+const noteCount = document.querySelectorAll(".count");
 
+// Input Value
 let avaliableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
 //Event Listner
 billAmount.addEventListener("keyup", getAmount);
 cashGiven.addEventListener("keyup", getCash);
 getChangeBtn.addEventListener("click", getChangeFn);
 
-for (i = 1; i <= 9; i++) {
-  document.getElementById("c" + i).style.backgroundColor =
-    "#" + Math.floor(Math.random() * 16777215).toString(16);
+for (i = 1; i <= 7; i++) {
+  let hex = Math.floor(Math.random() * 16777215).toString(16);
+  document.getElementById("c" + i).style.color = "#" + hex;
+  document.getElementById("inner-c" + i).style.borderColor = "#" + hex;
 }
 
 // Functions
 function getAmount(event) {
-  let amt = event.target.value;
-  if (amt <= 0) {
+  if (event.target.value <= 0) {
     amtWarning.style.visibility = "visible";
+    cashGiven.style.visibility = "hidden";
   } else {
-    cashGiven.style.visibility = "visible";
     amtWarning.style.visibility = "hidden";
+    cashGiven.style.visibility = "visible";
   }
 }
 
 function getCash(event) {
-  let cash = event.target.value;
-
-  if (cash < billAmount.value) {
+  if (event.target.value < billAmount.value) {
     cashWarning.style.visibility = "visible";
   } else {
     cashWarning.style.visibility = "hidden";
-    getChangeBtn.style.visibility = "visible";
   }
 }
 function getChangeFn(event) {
@@ -49,6 +49,5 @@ function getChangeFn(event) {
     }
   } else {
     cashWarning.style.visibility = "visible";
-    getChangeBtn.style.visibility = "hidden";
   }
 }
